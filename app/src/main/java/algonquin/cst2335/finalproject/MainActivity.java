d947fd01d79626a9228579050ae646b7d456b8cc
ehashi98 <hash0157@algonquinlive.com>
2022-12-07 15:55:10 -0500

    merging

--- app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
@@ -1,8 +1,12 @@
 package algonquin.cst2335.finalproject;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import View.SearchMovie;
+import algonquin.cst2335.finalproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding.Movies.setOnClickListener(click -> {
+            Intent moviePage = new Intent(this, SearchMovie.class);
+            startActivity(moviePage);
+        });
+
+        binding.Pexels.setOnClickListener(click -> {
+            Intent pexelsPage = new Intent(this, PexelsActivity.class);
+            startActivity(pexelsPage);
+        });
     }
 }
